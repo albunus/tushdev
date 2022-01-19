71f43339659b6794ba25614c05f6b54ab2685104
albunus <albunus.nyalita@student.moringaschool.com>
2022-01-19 11:47:45 +0300

    added contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SendMessageService } from 'src/app/send-message.service';
 
 @Component({
   selector: 'app-contact',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messageService: SendMessageService) {}
+
+  showSuccessMessage = false;
 
   ngOnInit(): void {
   }
 
-}
+
+
+  sendMessage(form: NgForm) {
+    const name = form.value.name;
+    const email = form.value.email;
+    const message = form.value.message;
+    const phone = form.value.phone;
+    const subject = form.value.subject;
+    this.messageService
+      .sendMessage(message, name, email, phone, subject)
+      .subscribe((response) => {
+        console.log(response);
+        this.showSuccessMessage = true;
+        setTimeout(() => (this.showSuccessMessage = false), 6000);
+        form.reset();
+      });
+  }
+  }
--- src/app/send-message.service.ts
+++ src/app/send-message.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SendMessageService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  sendMessage(
+    message: string,
+    name: string,
+    email: string,
+    phone: string,
+    subject: string
+  ) {
+    const data = {
+      message: message,
+      name: name,
+      email: email,
+      phone: phone,
+      subject: subject,
+    };
+    return this.http.post(
+      'https://my-portfolio-website-82420-default-rtdb.firebaseio.com/messages.json',
+      data
+    );
+  }
 }
+
