810cb16aa3322278129e9c579594309cc3415174
albunus <albunus.nyalita@student.moringaschool.com>
2022-01-19 11:56:07 +0300

    added footer and header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,4 +12,23 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  // onclick of button remove class from all divs having class 'active'
+  removeVisibilityClass() {
+    const divs = document.getElementsByClassName('remove-visibility');
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].classList.remove('remove-visibility');
+    }
+  }
+  addvisibilityClass() {
+    const divs = document.getElementsByClassName('new-to-be-added');
+    for (let i = 0; i < divs.length; i++) {
+      divs[i].classList.add('remove-visibility');
+    }
+  }
+
 }
+
+
+
+
+
